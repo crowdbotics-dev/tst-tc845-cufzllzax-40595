10a8f5c9f8ff6185161e602a09d9a8febc00eef8
Crowdbotics <engineering@crowdbotics.com>
2022-10-25 10:13:40 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Addressumwghdbygs
+admin.site.register(Addressumwghdbygs)
 
 # Register your models here.
--- backend/home/api/v1/urls.py
+++ backend/home/api/v1/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from .viewsets import AddressumwghdbygsViewSet
 
 from home.api.v1.viewsets import (
     SignupViewSet,
@@ -9,6 +10,7 @@ from home.api.v1.viewsets import (
 router = DefaultRouter()
 router.register("signup", SignupViewSet, basename="signup")
 router.register("login", LoginViewSet, basename="login")
+router.register('addressumwghdbygs', AddressumwghdbygsViewSet )
 
 urlpatterns = [
     path("", include(router.urls)),
